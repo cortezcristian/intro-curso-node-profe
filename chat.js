8e891d9349f07115c4eb65afc93c2c9ac4cffffd
Cristian Cortez <cortez.cristian@gmail.com>
2015-10-02 09:44:51 -0300

    Creando archivo chat

---
+++ chat.js
@@ -0,0 +1,22 @@
+var net = require('net');
+var sockets = [];
+
+var server = net.createServer(function(s){
+    // Definir que hago con cada socket s    
+    sockets.push(s);
+    s.write("Bienvenido al chat");
+
+    s.on('data', function(d){
+        sockets.forEach(function(v,i){
+          v.write(d);    
+        });
+    });
+
+    s.on("end", function(){
+      var index = sockets.indexof(s);
+      delete sockets[index];
+    });
+});
+
+server.listen(1337, '192.168.1.71');
+
