8b244c0362c5d0bc31abea37cbb13edcd3897562
Cristian Cortez <cortez.cristian@gmail.com>
2015-10-30 10:03:47 -0300

    Agregando el Modelo al Controlador

--- crudtest/routes/main.js
+++ crudtest/routes/main.js
@@ -1,7 +1,10 @@
 var app = module.parent.exports.app;
+var Persons = require('../models/persons.js');
 
 app.get('/list', function(req, res){
-  res.end('It works!');
+  Persons.find({}, function(err, docs){
+    res.json(docs);
+  });
 });
 
 app.post('/list', function(req, res){
